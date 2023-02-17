376eef79eb4527ac033791bbbdbd2fe239ee31d5
Benjamin Bech <bbe@jyskebank.dk>
2023-02-17 12:07:07 +0100

    changes

--- app/main.R
+++ app/main.R
@@ -1,23 +1,38 @@
 box::use(
-  shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput],
+  shiny[h1, bootstrapPage, div, moduleServer, NS, tags, icon],
 )
 
 box::use(
   app/view/chart,
+  app/view/table,
 )
+
+
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
   
   bootstrapPage(
-    chart$ui(ns("chart"))
+    h1("RhinoApplication"),
+    div(
+      class = "components-container",
+      table$ui(ns("table")),
+      chart$ui(ns("chart")) 
+    ),
+    tags$button(
+      id = "help-button",
+      icon("question")
+    )
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    chart$server("chart")
+    data <- rhino::rhinos
+    
+    table$server("table", data = data)
+    chart$server("chart", data = data)
   })
 }
--- app/view/chart.R
+++ app/view/chart.R
@@ -1,17 +1,36 @@
 box::use(
-  shiny[h3, moduleServer, NS],
+  echarts4r,
+  shiny[h3, moduleServer, NS, div],
+)
+
+box::use(
+  app/logic/chart_utils[label_formatter],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
   
-  h3("Chart")
+  div(
+    class = "component-box",
+    echarts4r$echarts4rOutput(ns("chart"))
+  )
 }
 
 #' @export
-server <- function(id) {
+server <- function(id, data) {
   moduleServer(id, function(input, output, session) {
-    print("Chart module server part works!")
+    output$chart <- echarts4r$renderEcharts4r(
+      data |>
+        echarts4r$group_by(Species) |>
+        echarts4r$e_chart(x = Year) |>
+        echarts4r$e_line(Population) |>
+        echarts4r$e_x_axis(
+          Year,
+          axisLabel = list(
+            formatter = label_formatter
+          )) |>
+        echarts4r$e_tooltip()
+    )
   })
 }
